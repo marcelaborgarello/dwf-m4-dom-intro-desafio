f0c8fe939f6f1033a4a4386fea5e56bc7e0fc854
imprenart <imprenart@gmail.com>
2022-06-06 09:43:15 -0300

    resuleto

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaItemsEl = document.querySelector(".lista");
+  const itemsEl = document.querySelectorAll(".lista li");
+
+  for (const item of itemsEl) {
+    item.remove();
+  }
+
+  for (const item of cosasQueAprendimos) {
+    const nuevoLiEl = document.createElement("li");
+    nuevoLiEl.textContent = item.tema;
+    nuevoLiEl.classList.add(item.class || "item");
+    listaItemsEl.appendChild(nuevoLiEl);
+  }
+}
 
 main();
